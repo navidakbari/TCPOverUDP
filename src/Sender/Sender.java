19b3c5ac32ec68bcacb1646cbde6cb1c921dfc1b
navid <anavid.akbari@gmail.com>
2019-04-24 15:58:35 +0430

    this commit will add SYN and ACK flag to TCP Socket

--- src/Sender/Sender.java
+++ src/Sender/Sender.java
@@ -1,10 +1,7 @@
-import java.io.IOException;
-import java.net.*;
-
 public class Sender {
     public static void main(String[] args) throws Exception {
-        TCPSocket tcpSocket = new TCPSocketImpl("127.0.0.1", 1234);
-        tcpSocket.send("./src/Sender/file.txt");
+        TCPSocket tcpSocket = new TCPSocketImpl("127.0.0.1", 9000);
+        tcpSocket.send("./src/Sender/file.txt" , "127.0.0.1" , 8080);
 //        tcpSocket.close();
 //        tcpSocket.saveCongestionWindowPlot();
     }
--- src/TCP/TCPPacket.java
+++ src/TCP/TCPPacket.java
@@ -1,40 +1,45 @@
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.InetAddress;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 
 public class TCPPacket {
-    private int seq;
-    private int ack;
-//    private boolean synAck;
+    private int sequenceNumber;
+    private int acknowledgmentNumber;
+    private boolean SYN;
+    private boolean ACK;
     private int destinationPort;
+    private String destinationIp;
     private int dataLength;
     private String data;
 
-    TCPPacket(int seq, int ack, int destinationPort, String pathToFile) throws IOException {
-        this.seq = seq;
-        this.ack = ack;
+    TCPPacket(String destinationIp , int destinationPort , int sequenceNumber, int acknowledgmentNumber,boolean ACK , boolean SYN , String data) throws IOException {
+        this.sequenceNumber = sequenceNumber;
+        this.acknowledgmentNumber = acknowledgmentNumber;
         this.destinationPort = destinationPort;
-        this.data = this.readDataFromFile(pathToFile);
+        this.destinationIp = destinationIp;
+        this.ACK = ACK;
+        this.SYN = SYN;
+        this.data = data;
         this.dataLength = data.length();
-//        this.synAck = synAck;
-    }
-
-    private String readDataFromFile(String pathToFile) throws IOException {
-        String data;
-        data = new String(Files.readAllBytes(Paths.get(pathToFile)));
-        return data;
     }
 
     TCPPacket(DatagramPacket dp) {
          this.destinationPort = dp.getPort();
          byte[] buff = dp.getData();
-         this.seq = byteToInt(0, buff);
-         this.ack = byteToInt(4, buff);
+         this.sequenceNumber = byteToInt(0, buff);
+         this.acknowledgmentNumber = byteToInt(4, buff);
          this.dataLength = byteToInt(8, buff);
-         this.data = new String(buff, 12, dataLength);
+         this.ACK = byteToBool(12 , buff);
+         this.SYN = byteToBool(13 , buff);
+         this.data = new String(buff, 14, dataLength);
+    }
 
+    private boolean byteToBool(int index, byte[] buff) {
+        if(buff[index] == (byte)1){
+            return true;
+        }else{
+            return false;
+        }
     }
 
     private int byteToInt(int index, byte[] buff){
@@ -44,11 +49,22 @@ public class TCPPacket {
 
     private byte[] createPacket(){
         byte[] packet = new byte[1408];
-        intToBytes(seq, 0, packet);
-        intToBytes(ack, 4, packet);
-        intToBytes(dataLength , 8, packet);
-        injectBytes(packet, 12 , data.getBytes());
+        intToBytes(this.sequenceNumber, 0, packet);
+        intToBytes(this.acknowledgmentNumber, 4, packet);
+        intToBytes(this.dataLength , 8, packet);
+        boolToByte(this.ACK , 12 , packet);
+        boolToByte(this.SYN , 13 , packet);
+        injectBytes(packet, 14 , data.getBytes());
         return packet;
+    }
+
+    private void boolToByte(final boolean data, int index, byte[] buff) {
+        if(data == true){
+            buff[index] = (byte)1;
+        }else{
+            buff[index] = (byte)0;
+        }
+
     }
 
     private void intToBytes(final int data, int index, byte[]buff) {
@@ -65,12 +81,20 @@ public class TCPPacket {
     public DatagramPacket getUDPPacket() throws Exception{
         byte[] buff = this.createPacket();
         DatagramPacket dp = new DatagramPacket(buff, buff.length );
-        dp.setPort(destinationPort);
-        dp.setAddress(InetAddress.getByName("127.0.0.1"));
+        dp.setPort(this.destinationPort);
+        dp.setAddress(InetAddress.getByName(this.destinationIp));
         return dp;
     }
 
     public String getData() {
         return data;
+    }
+
+    public Boolean getSynFlag(){
+        return SYN;
+    }
+
+    public Boolean getAckFlag(){
+        return ACK;
     }
 }
