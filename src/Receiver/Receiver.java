ca25b9e56c8f8877461c3143041ca6051a8aa67b
navid <anavid.akbari@gmail.com>
2019-05-12 17:51:41 +0430

    this commit will add congestion window state and implement  slow start state

--- src/Receiver/Receiver.java
+++ src/Receiver/Receiver.java
@@ -1,6 +1,6 @@
 public class Receiver {
     public static void main(String[] args) throws Exception {
-        TCPServerSocket tcpServerSocket = new TCPServerSocketImpl(8080);
+        TCPServerSocket tcpServerSocket = new TCPServerSocketImpl(80);
         TCPSocket tcpSocket = tcpServerSocket.accept();
         tcpSocket.receive("./src/Receiver/result.png" );
 //        tcpSocket.receive("receiving.mp3");
